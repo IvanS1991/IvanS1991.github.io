aca473a3ef44ec055b3af83c45c4b5216d5d9e2b
Ivan Shtarbanov <haifischy91@gmail.com>
2017-11-05 18:19:41 +0200

    Another fix, clean-css seems to break the app for mobile

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ gulp.task('compile:scss', ['clean:css'], () => {
   return gulp.src(`src/scss/**/*.scss`)
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('styles.css'))
-    .pipe(cleanCss())
     .pipe(gulp.dest('dist/css'));
 });
 
