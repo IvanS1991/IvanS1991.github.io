2be124261666add5878dc499007f45b153b2ac27
Ivan Shtarbanov <ivan.shtarbanov@edited.com>
2022-02-18 18:23:35 +0200

    Update page content

--- src/ts/components/nav.ts
+++ src/ts/components/nav.ts
@@ -1,31 +1,38 @@
-import 'jquery';
-import { emitter } from '../page-events';
+import "jquery";
+import { emitter } from "../page-events";
 
 const init = (selectors): void => {
-  const $nav:JQuery = $(`.${selectors.nav}`);
+  const $nav: JQuery = $(`.${selectors.nav}`);
   const $navContainer: JQuery = $(`.${selectors.container}`);
   const $mobileTrigger: JQuery = $(`.${selectors.mobile}`);
+  const $languageSwitch: JQuery = $(`.${selectors.langSwitch}`);
   const $buttons: JQuery = $(`.${selectors.nav} .${selectors.button}`);
   const classActive: string = `${selectors.button}_active`;
   const classExpanded: string = `${selectors.nav}__container__is-expanded`;
-  $nav.on('click', (event): void => {
+  $nav.on("click", (event): void => {
     event.preventDefault();
     const $this: JQuery = $(event.target);
-    const currentPage = $this.attr('data-page');
+    const currentPage = $this.attr("data-page");
     if ($this.hasClass(selectors.button)) {
-      emitter.emit('page-change', currentPage);
+      emitter.emit("page-change", currentPage);
       $buttons.removeClass(classActive);
       $this.addClass(classActive);
       $navContainer.removeClass(classExpanded);
-      sessionStorage.setItem('currentPage', currentPage);
+      sessionStorage.setItem("currentPage", currentPage);
     }
   });
 
-  $mobileTrigger.on('click', $mobileTrigger, (event): void => {
+  $mobileTrigger.on("click", (event): void => {
     event.preventDefault();
     const $this: JQuery = $(event.target);
     const $navContainer = $this.next();
     $navContainer.toggleClass(classExpanded);
+  });
+
+  $languageSwitch.on("click", (event): void => {
+    event.preventDefault();
+    const $this: JQuery = $(event.target);
+    console.log($this);
   });
 };
 
